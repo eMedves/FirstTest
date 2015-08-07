b164e9da0372862edcecc4ef60e6fd3e105eac50
enrmedve <enrico.medves@eng.it>
2015-08-07 07:11:07 +0000

    Added parameter "ignoreFaultStrings" to change how JBFHelper handles errors

--- spagic3-ebpm/org.spagic3.components.jbf/src/org/spagic3/connectors/jbf/helpers/JBFCaller.java
+++ spagic3-ebpm/org.spagic3.components.jbf/src/org/spagic3/connectors/jbf/helpers/JBFCaller.java
@@ -10,14 +10,14 @@ import org.eclipse.ebpm.util.resources.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class JBFCaller {
-	
+
 	protected Logger logger = LoggerFactory.getLogger(JBFCaller.class);
-	
+
 	private Properties properties = null;
-	private boolean balanced  = false;
-	
+	private boolean balanced = false;
+	private boolean ignoreFaultStrings = false;
+
 	public boolean isBalanced() {
 		return balanced;
 	}
@@ -26,90 +26,97 @@ public class JBFCaller {
 		this.balanced = balanced;
 	}
 
-	public JBFCaller(String spagicUri){
-		this(spagicUri,false);
+	public JBFCaller(String spagicUri) {
+		this(spagicUri, false);
 	}
-	
-	public JBFCaller(Properties properties){
-		this(properties,false);
+
+	public JBFCaller(Properties properties) {
+		this(properties, false);
 	}
-	
-	public JBFCaller(String spagicUri,  boolean balanced){
+
+	public JBFCaller(String spagicUri, boolean balanced) {
+		this(spagicUri, balanced, false);
+	}
+
+	public JBFCaller(Properties properties, boolean balanced) {
+		this(properties, balanced, false);
+	}
+
+	public JBFCaller(Properties properties, boolean balanced, boolean ignoreFaultStrings) {
+		this.properties = properties;
+		this.balanced = balanced;
+		this.ignoreFaultStrings = ignoreFaultStrings;
+	}
+
+	public JBFCaller(String spagicUri, boolean balanced, boolean ignoreFaultStrings) {
 		IResource resource = new Resource(spagicUri);
 		this.properties = new Properties();
-		try{
+		try {
 			properties.load(resource.openStream());
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		this.balanced = balanced;
-		
+		this.ignoreFaultStrings = ignoreFaultStrings;
 	}
-	
-	
-	
-	public JBFCaller(Properties properties, boolean balanced){
-		this.properties = properties;
-		this.balanced = balanced;
-	}
-	
-	public boolean  handleError( int code, JBFHelper helper, ProcessContext pc, Exchange exchange, Message in, Message out){
 
-		if (code == JBFConstants.JBF_CONNECTION_ERROR){
+	public boolean handleError(int code, JBFHelper helper, ProcessContext pc, Exchange exchange,
+			Message in, Message out) {
+
+		if (code == JBFConstants.JBF_CONNECTION_ERROR) {
 			String error = helper.getJbfError();
 			out.setHeader(JBFConstants.JBF_ESITO_HEADER_NAME, JBFConstants.KO);
 			out.setHeader(JBFConstants.JBF_ERRORE_HEADER_NAME, error);
-			
+
 			pc.setVariable(JBFConstants.JBF_ESITO_HEADER_NAME, JBFConstants.KO);
 			pc.setVariable(JBFConstants.JBF_ERRORE_HEADER_NAME, error);
 			out.setBodyText(helper.getJbfResponse());
-			
+
 			return true;
-		}else if (code == JBFConstants.JBF_APPLICATION_ERROR){
+		} else if (code == JBFConstants.JBF_APPLICATION_ERROR) {
 			String error = helper.getJbfError();
 			out.setHeader(JBFConstants.JBF_ESITO_HEADER_NAME, JBFConstants.KO);
 			out.setHeader(JBFConstants.JBF_ERRORE_HEADER_NAME, error);
-			
+
 			pc.setVariable(JBFConstants.JBF_ESITO_HEADER_NAME, JBFConstants.KO);
 			pc.setVariable(JBFConstants.JBF_ERRORE_HEADER_NAME, error);
 			out.setBodyText(helper.getJbfResponse());
-			
+
 			return true;
-		}else{
+		} else {
 			return false;
 		}
-		
+
 	}
-	public void call(Exchange exchange, Message in, Message out){
+
+	public void call(Exchange exchange, Message in, Message out) {
 		ProcessContext pc = new ProcessContext(exchange);
 		JBFHelper jbfHelper = new JBFHelper(properties);
 		jbfHelper.setBalanced(balanced);
-	
-		// OPEN SESSION 
+		jbfHelper.setIgnoreFaultStrings(ignoreFaultStrings);
+		
+		// OPEN SESSION
 		logger.debug("-- JBFCaller -- Opening session");
 		int openSession = jbfHelper.openSession();
 		if (handleError(openSession, jbfHelper, pc, exchange, in, out))
 			return;
-		
-	
-		
+
 		// CALL
 		logger.debug("-- JBFCaller -- Calling WS");
 		jbfHelper.setJbfRequest(in.getBodyText());
 		int call = jbfHelper.callWS();
 		if (handleError(call, jbfHelper, pc, exchange, in, out))
 			return;
-		
+
 		out.setHeader(JBFConstants.JBF_ESITO_HEADER_NAME, JBFConstants.OK);
 		pc.setVariable(JBFConstants.JBF_ESITO_HEADER_NAME, JBFConstants.OK);
 		out.setBodyText(jbfHelper.getJbfResponse());
-		
-		
+
 		// CLOSE SESSION
 		logger.debug("-- JBFCaller -- Closing session");
 		int closeSession = jbfHelper.closeSession();
 		if (handleError(closeSession, jbfHelper, pc, exchange, in, out))
 			return;
-		
+
 	}
 }
--- spagic3-ebpm/org.spagic3.components.jbf/src/org/spagic3/connectors/jbf/helpers/JBFHelper.java
+++ spagic3-ebpm/org.spagic3.components.jbf/src/org/spagic3/connectors/jbf/helpers/JBFHelper.java
@@ -29,376 +29,365 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JBFHelper {
-	
+
 	protected Logger logger = LoggerFactory.getLogger(JBFHelper.class);
 
 	private Properties properties = null;
-	private boolean balanced  = false;
-	
+	private boolean balanced = false;
+	private boolean ignoreFaultStrings = false;
+
 	private String sessionId = null;
-	private String cookieLblSessionId = null; //GLC add
+	private String cookieLblSessionId = null; // GLC add
 	private String cookieOpenSession = null;
-	
+
 	private String jbfRequest;
 	private String jbfResponse;
 	private String jbfError;
 
-	
 	public String getCookieLblSessionId() {
 		return cookieLblSessionId;
 	}
-
 
 	public void setCookieLblSessionId(String cookieLblSessionId) {
 		this.cookieLblSessionId = cookieLblSessionId;
 	}
 
-	
 	public String getJbfError() {
 		return jbfError;
 	}
-
-
-
 
 	public void setJbfError(String jbfError) {
 		this.jbfError = jbfError;
 	}
 
-
-
-
 	public Properties getProperties() {
 		return properties;
 	}
-
-
-
 
 	public void setProperties(Properties properties) {
 		this.properties = properties;
 	}
 
-
-
-
 	public String getJbfRequest() {
 		return jbfRequest;
 	}
-
-
-
 
 	public void setJbfRequest(String jbfRequest) {
 		this.jbfRequest = jbfRequest;
 	}
 
-
-
-
 	public String getJbfResponse() {
 		return jbfResponse;
 	}
-
-
-
 
 	public void setJbfResponse(String jbfResponse) {
 		this.jbfResponse = jbfResponse;
 	}
 
+	public boolean isIgnoreFaultStrings() {
+		return ignoreFaultStrings;
+	}
 
-
+	public void setIgnoreFaultStrings(boolean ignoreFaultStrings) {
+		this.ignoreFaultStrings = ignoreFaultStrings;
+	}
 
 	public JBFHelper(Properties properties) {
 		this.properties = properties;
 	}
-	
-	
-	public String getProperty(String key){
-		System.out.println(" JBF Helper -> Getting Property ["+key+"]");
-		return (this.properties.getProperty(key) != null ? this.properties.getProperty(key) : "" );
-	}
-	
-	public int openSession(){
-		try{
-			
-			logger.debug("-- JBFHelper -- Opening session");
-		
-         Document openSessionDoc = DocumentHelper.createDocument();
-         
-         
-//         <Input>
-//         		<OpenSession>
-//         		<user>XMPI</user>
-//         		<password>XMPIXMPI</password>
-//         		<azienda></azienda>
-//         		<ufficio></ufficio>
-//         		<dataLavoro></dataLavoro>
-//         		<entita>XMPI</entita>
-//         		<OpenSession>
-//         <Input>
-         		
-         		
-         		
-         Element inputElement = openSessionDoc.addElement("Input");
-         Element openSession = inputElement.addElement("OpenSession");
-         
-         Element userEl = openSession.addElement("user");
-         userEl.setText(getProperty(JBFConstants.JBF_USER));
-         Element passwordEl = openSession.addElement("password");
-         passwordEl.setText(getProperty(JBFConstants.JBF_PASSWORD));
-         Element aziendaEl = openSession.addElement("azienda");
-         aziendaEl.setText(getProperty(JBFConstants.JBF_AZIENDA));
-         Element ufficioEl = openSession.addElement("ufficio");
-         ufficioEl.setText(getProperty(JBFConstants.JBF_UFFICIO));
-         Element dataLavoroEl = openSession.addElement("dataLavoro");
-         dataLavoroEl.setText("");
-         Element entitaEl = openSession.addElement("entita");
-         entitaEl.setText(getProperty(JBFConstants.JBF_ENTITA));
 
-         
-         SOAPMessage jbfSOAPCallRequest = createJBFSoapMessageRequest(inputElement);
-   		 
-         logger.debug("-- JBFHelper -- Soap Call");
-   		 SOAPMessage response = call(this.properties.getProperty(JBFConstants.JBF_URL),jbfSOAPCallRequest);
-   		 
-   		 
-   		 String body = getBodyAsString(response);
-   		 
-   		logger.debug("-- JBFHelper -- Response From Open Session \n{}", body);
-   		 System.out.println( " Response From Open Session ");
-   		 System.out.println(body);
-   		 
-//   		 // get cookie per bilanciamento joem 30 11 2009
-//			CookieOpenSession = 
-//					response.getMimeHeaders().getHeader("Set-Cookie");
-//			System.out.println("\nThe COOKIE OPEN SESSION IS : .... : "+ 
-//					CookieOpenSession[0]);
-//			// get cookie per bilanciamento joem 30 11 2009
-   		 
+	public String getProperty(String key) {
+		System.out.println(" JBF Helper -> Getting Property [" + key + "]");
+		return (this.properties.getProperty(key) != null ? this.properties.getProperty(key) : "");
+	}
+
+	public int openSession() {
+		try {
+
+			logger.debug("-- JBFHelper -- Opening session");
+
+			Document openSessionDoc = DocumentHelper.createDocument();
+
+			// <Input>
+			// <OpenSession>
+			// <user>XMPI</user>
+			// <password>XMPIXMPI</password>
+			// <azienda></azienda>
+			// <ufficio></ufficio>
+			// <dataLavoro></dataLavoro>
+			// <entita>XMPI</entita>
+			// <OpenSession>
+			// <Input>
+
+			Element inputElement = openSessionDoc.addElement("Input");
+			Element openSession = inputElement.addElement("OpenSession");
+
+			Element userEl = openSession.addElement("user");
+			userEl.setText(getProperty(JBFConstants.JBF_USER));
+			Element passwordEl = openSession.addElement("password");
+			passwordEl.setText(getProperty(JBFConstants.JBF_PASSWORD));
+			Element aziendaEl = openSession.addElement("azienda");
+			aziendaEl.setText(getProperty(JBFConstants.JBF_AZIENDA));
+			Element ufficioEl = openSession.addElement("ufficio");
+			ufficioEl.setText(getProperty(JBFConstants.JBF_UFFICIO));
+			Element dataLavoroEl = openSession.addElement("dataLavoro");
+			dataLavoroEl.setText("");
+			Element entitaEl = openSession.addElement("entita");
+			entitaEl.setText(getProperty(JBFConstants.JBF_ENTITA));
+
+			SOAPMessage jbfSOAPCallRequest = createJBFSoapMessageRequest(inputElement);
+
+			logger.debug("-- JBFHelper -- Soap Call");
+			SOAPMessage response = call(this.properties.getProperty(JBFConstants.JBF_URL),
+					jbfSOAPCallRequest);
+
+			String body = getBodyAsString(response);
+
+			logger.debug("-- JBFHelper -- Response From Open Session \n{}", body);
+			System.out.println(" Response From Open Session ");
+			System.out.println(body);
+
+			// // get cookie per bilanciamento joem 30 11 2009
+			// CookieOpenSession =
+			// response.getMimeHeaders().getHeader("Set-Cookie");
+			// System.out.println("\nThe COOKIE OPEN SESSION IS : .... : "+
+			// CookieOpenSession[0]);
+			// // get cookie per bilanciamento joem 30 11 2009
+
 			if (isBalanced()) {
-				
-				this.cookieOpenSession = response.getMimeHeaders().getHeader(
-						"Set-Cookie")[0];
-				//GLC
+
+				this.cookieOpenSession = response.getMimeHeaders().getHeader("Set-Cookie")[0];
+				// GLC
 				int counterHeader = response.getMimeHeaders().getHeader("Set-Cookie").length;
-				if(counterHeader>1){
+				if (counterHeader > 1) {
 					this.cookieLblSessionId = response.getMimeHeaders().getHeader("Set-Cookie")[1];
 					System.out.println("\nGLC: .... : " + this.cookieLblSessionId);
 				}
-				//GLC controllo se esiste LBL nei coockie
-				//NB. se dovranno essere passati piu coockie, far diventare cookieOpenSession come String [], e usare il for each:
-				//for(String fullCookieStr : response.getMimeHeaders().getHeader("Set-Cookie") )
-				//		System.out.println("\nGLC: .... : " + fullCookieStr);
-				//this.lblSessionId = response.getMimeHeaders().getHeader("lblSessionId")[0];
+				// GLC controllo se esiste LBL nei coockie
+				// NB. se dovranno essere passati piu coockie, far diventare
+				// cookieOpenSession come String [], e usare il for each:
+				// for(String fullCookieStr :
+				// response.getMimeHeaders().getHeader("Set-Cookie") )
+				// System.out.println("\nGLC: .... : " + fullCookieStr);
+				// this.lblSessionId =
+				// response.getMimeHeaders().getHeader("lblSessionId")[0];
 			}
-			
-			
+
 			if (isErrorResponse(body)) {
 				logger.debug("-- JBFHelper -- OpenSession Response contains an error");
 				setJbfError(retrieveConnectionError(body));
 				setJbfResponse(connectionRefused());
 				return JBFConstants.JBF_CONNECTION_ERROR;
-				
+
 			} else if (isException(body)) {
 				logger.debug("-- JBFHelper -- OpenSession Response contains an exception");
 				setJbfError(retrieveExceptionDescription(body));
 				setJbfResponse(retrieveRealResponse(body));
 				return JBFConstants.JBF_APPLICATION_ERROR;
-				
+
 			} else {
 				logger.debug("-- JBFHelper -- OpenSession Response is OK");
 				this.sessionId = retrieveSessionId(body);
-				//this.lblSessionId = retrieveLBLSessionId(body); //GLC non serve lato app
+				// this.lblSessionId = retrieveLBLSessionId(body); //GLC non
+				// serve lato app
 				return JBFConstants.JBF_OK;
 			}
 		} catch (Throwable e) {
 			throw new RuntimeException("Error Invoking Spagic", e);
 		}
 	}
-	public int callWS(){
-		try{
-			String payloadSessionId = this.jbfRequest.replace("SESSIONEJBFID",this.sessionId);
-		
+
+	public int callWS() {
+		try {
+			String payloadSessionId = this.jbfRequest.replace("SESSIONEJBFID", this.sessionId);
+
 			Document payloadDoc = DocumentHelper.parseText(payloadSessionId);
-			SOAPMessage jbfSOAPCallRequest = createJBFSoapMessageRequest(payloadDoc.getRootElement());
-	   		SOAPMessage response = call(this.properties.getProperty(JBFConstants.JBF_URL),jbfSOAPCallRequest);
-	   		
-	   		
-	   		String body = getBodyAsString(response);
-	   		logger.debug("-- JBFHelper -- Received response: {}", body);
-	   		if (isErrorResponse(body)){
-	   			logger.debug("-- JBFHelper -- Response contains an error");
-	   			setJbfError(retrieveConnectionError(body));
-	   			setJbfResponse(connectionRefused());
-	   			return JBFConstants.JBF_CONNECTION_ERROR;
-			}else if (isException(body)){
+			SOAPMessage jbfSOAPCallRequest = createJBFSoapMessageRequest(payloadDoc
+					.getRootElement());
+			SOAPMessage response = call(this.properties.getProperty(JBFConstants.JBF_URL),
+					jbfSOAPCallRequest);
+
+			String body = getBodyAsString(response);
+			logger.debug("-- JBFHelper -- Received response: {}", body);
+			if (isErrorResponse(body)) {
+				logger.debug("-- JBFHelper -- Response contains an error");
+				setJbfError(retrieveConnectionError(body));
+				setJbfResponse(connectionRefused());
+				return JBFConstants.JBF_CONNECTION_ERROR;
+			} else if (isException(body)) {
 				logger.debug("-- JBFHelper -- Response contains an exception");
 				setJbfError(retrieveExceptionDescription(body));
 				setJbfResponse(retrieveRealResponse(body));
 				return JBFConstants.JBF_APPLICATION_ERROR;
-	   		}else{
-	   			logger.debug("-- JBFHelper -- Response is OK");
-	   			 this.jbfResponse = retrieveRealResponse(body);
-	   			 return JBFConstants.JBF_OK;
-	   		}
-		}catch (Throwable e) {
+			} else {
+				logger.debug("-- JBFHelper -- Response is OK");
+				this.jbfResponse = retrieveRealResponse(body);
+				return JBFConstants.JBF_OK;
+			}
+		} catch (Throwable e) {
 			throw new RuntimeException("Error Invoking JBF::callWS", e);
 		}
 	}
-	
-	public String connectionRefused(){
+
+	public String connectionRefused() {
 		return "<Output><Exception><Description>Connection refused</Description></Exception></Output>";
 	}
+
 	public int closeSession() {
 		try {
 			Document openSessionDoc = DocumentHelper.createDocument();
 			Element inputElement = openSessionDoc.addElement("Input");
 			inputElement.addAttribute("SessionID", this.sessionId);
-			//GLC serve ache per la chiusura? viene aggiunto nella createJBFSoapMessageRequest
-			//inputElement.addAttribute("lblSessionID", this.lblSessionId);
+			// GLC serve ache per la chiusura? viene aggiunto nella
+			// createJBFSoapMessageRequest
+			// inputElement.addAttribute("lblSessionID", this.lblSessionId);
 			inputElement.addElement("CloseSession");
 
 			SOAPMessage jbfSOAPCallRequest = createJBFSoapMessageRequest(inputElement);
 
-			SOAPMessage response = call(this.properties.getProperty(JBFConstants.JBF_URL), jbfSOAPCallRequest);
+			SOAPMessage response = call(this.properties.getProperty(JBFConstants.JBF_URL),
+					jbfSOAPCallRequest);
 
 		} catch (Throwable e) {
 			throw new RuntimeException("Error Invoking JBF::closeSession", e);
 		}
 		return JBFConstants.JBF_OK;
 	}
-	
+
 	public String getSessionId() {
 		return sessionId;
 	}
+
 	public void setSessionId(String sessionId) {
 		this.sessionId = sessionId;
 	}
+
 	public String getCookieOpenSession() {
 		return cookieOpenSession;
 	}
+
 	public void setCookieOpenSession(String cookieOpenSession) {
 		this.cookieOpenSession = cookieOpenSession;
 	}
 
-	
 	public SOAPMessage createJBFSoapMessageRequest(Element el) throws Exception {
-		
-		 System.out.println("Input JBF");
-		 System.out.println(el.asXML());
-		 MessageFactory messageFactory = MessageFactory.newInstance();
-         SOAPMessage message = messageFactory.createMessage();
-         SOAPEnvelope envelope = message.getSOAPPart().getEnvelope();
-         SOAPHeader soapHeader = envelope.getHeader();
-         
-         SOAPBody soapBody = envelope.getBody();
-         
-         SOAPBodyElement callElement;
-		 SOAPElement requestElement;
-   		 callElement= soapBody.addBodyElement(envelope.createName("call"));
-   		 requestElement= callElement.addChildElement("callRequest");
-   		 requestElement.addTextNode(el.asXML());
-   		 message.getMimeHeaders().addHeader("SOAPAction", "");
-   		 
-   		 if ((this.cookieOpenSession != null) && isBalanced()){
-   			message.getMimeHeaders().setHeader("Cookie", cookieOpenSession);
-   			//GLC add
-   			message.getMimeHeaders().addHeader("Cookie", cookieLblSessionId);
-   		 }
-   		
-   		 return message;
+
+		System.out.println("Input JBF");
+		System.out.println(el.asXML());
+		MessageFactory messageFactory = MessageFactory.newInstance();
+		SOAPMessage message = messageFactory.createMessage();
+		SOAPEnvelope envelope = message.getSOAPPart().getEnvelope();
+		SOAPHeader soapHeader = envelope.getHeader();
+
+		SOAPBody soapBody = envelope.getBody();
+
+		SOAPBodyElement callElement;
+		SOAPElement requestElement;
+		callElement = soapBody.addBodyElement(envelope.createName("call"));
+		requestElement = callElement.addChildElement("callRequest");
+		requestElement.addTextNode(el.asXML());
+		message.getMimeHeaders().addHeader("SOAPAction", "");
+
+		if ((this.cookieOpenSession != null) && isBalanced()) {
+			message.getMimeHeaders().setHeader("Cookie", cookieOpenSession);
+			// GLC add
+			message.getMimeHeaders().addHeader("Cookie", cookieLblSessionId);
+		}
+
+		return message;
 	}
-	
-	
-	
+
 	public String retrieveSessionId(String body) throws Exception {
 		Document bodyDocument = DocumentHelper.parseText(body);
 		org.dom4j.Node callret = bodyDocument.selectSingleNode("/callResponse/callReturn");
 		String callRetText = callret.getText();
-		
+
 		Document callRetDoc = DocumentHelper.parseText(callRetText);
 		org.dom4j.Node sessIdDoc = callRetDoc.selectSingleNode("/Output/OpenSession/SessionID");
-		
+
 		return sessIdDoc.getText();
-		
+
 	}
-	//GLC LBL non serve lato app
-/*	public String retrieveLBLSessionId(String body) throws Exception {
-		Document bodyDocument = DocumentHelper.parseText(body);
-		org.dom4j.Node callret = bodyDocument.selectSingleNode("/callResponse/callReturn");
-		String callRetText = callret.getText();
-		
-		Document callRetDoc = DocumentHelper.parseText(callRetText);
-		org.dom4j.Node lblSessIdDoc = callRetDoc.selectSingleNode("/Output/OpenSession/lblSessionID");
-		
-		return lblSessIdDoc.getText();
-		
-	}	*/
+
+	// GLC LBL non serve lato app
+	/*
+	 * public String retrieveLBLSessionId(String body) throws Exception {
+	 * Document bodyDocument = DocumentHelper.parseText(body); org.dom4j.Node
+	 * callret = bodyDocument.selectSingleNode("/callResponse/callReturn");
+	 * String callRetText = callret.getText();
+	 * 
+	 * Document callRetDoc = DocumentHelper.parseText(callRetText);
+	 * org.dom4j.Node lblSessIdDoc =
+	 * callRetDoc.selectSingleNode("/Output/OpenSession/lblSessionID");
+	 * 
+	 * return lblSessIdDoc.getText();
+	 * 
+	 * }
+	 */
 	public String retrieveRealResponse(String body) throws Exception {
 		Document bodyDocument = DocumentHelper.parseText(body);
 		org.dom4j.Node callret = bodyDocument.selectSingleNode("/callResponse/callReturn");
 		String callRetText = callret.getText();
-		
+
 		Document callRetDoc = DocumentHelper.parseText(callRetText);
-	
-		
+
 		return callRetDoc.getRootElement().asXML();
-		
+
 	}
-	
+
 	public String retrieveExceptionDescription(String body) throws Exception {
 		Document bodyDocument = DocumentHelper.parseText(body);
 		org.dom4j.Node callret = bodyDocument.selectSingleNode("/callResponse/callReturn");
 		String callRetText = callret.getText();
-		
+
 		Document callRetDoc = DocumentHelper.parseText(callRetText);
-	
+
 		Node exDescription = callRetDoc.selectSingleNode("/Output/Exception/Description");
-		
+
 		return exDescription != null ? exDescription.getText() : "Generic Exception";
-	
+
 	}
-	
+
 	public String retrieveConnectionError(String body) throws Exception {
-		
-		
+
 		return "JBF Connection Refused";
-	
+
 	}
-	public boolean isErrorResponse(String body){
-		return false;
-		// removed because Areas Web Services often do not use a standard to communicate faults
-//		return body.indexOf("faultstring")>0;
+
+	public boolean isErrorResponse(String body) {
+		if (isIgnoreFaultStrings())
+			return false;
+		else
+			return body.indexOf("faultstring") > 0;
 	}
-	
-	public boolean isException(String body){
-		return false;
-		// removed because Areas Web Services often do not use a standard to communicate exceptions
-//		return body.indexOf("Exception")>0;
+
+	public boolean isException(String body) {
+		if (isIgnoreFaultStrings())
+			return false;
+		else
+			return body.indexOf("Exception") > 0;
 	}
+
 	public static SOAPMessage call(String wsEndpoint, SOAPMessage soapRequest) throws SOAPException {
 		SOAPConnection soapConnection = null;
-		try{
-			SOAPConnectionFactory soapConnectionFactory = javax.xml.soap.SOAPConnectionFactory.newInstance();
+		try {
+			SOAPConnectionFactory soapConnectionFactory = javax.xml.soap.SOAPConnectionFactory
+					.newInstance();
 
 			soapConnection = soapConnectionFactory.createConnection();
 
-			
 			javax.xml.soap.SOAPMessage soapResponse = soapConnection.call(soapRequest, wsEndpoint);
 			return soapResponse;
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
-		}finally{
+		} finally {
 			soapConnection.close();
 		}
-		
 
 	}
+
 	private String getBodyAsString(SOAPMessage soapMessage) throws Exception {
-		
 
 		// This is the "jbfRequest" element
 		SOAPBody body = soapMessage.getSOAPBody();
@@ -406,29 +395,24 @@ public class JBFHelper {
 		Transformer transformer = transFactory.newTransformer();
 		StringWriter buffer = new StringWriter();
 		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-		transformer.transform(new DOMSource(body.getFirstChild()),new StreamResult(buffer));
+		transformer.transform(new DOMSource(body.getFirstChild()), new StreamResult(buffer));
 		String str = buffer.toString();
-		
+
 		return str;
-	
+
 	}
-	
-	
-	
-	
-	
-	
-	protected String getSoapFaultDetail(SOAPBody body){
+
+	protected String getSoapFaultDetail(SOAPBody body) {
 		SOAPFault fault = body.getFault();
-		if (fault != null){
+		if (fault != null) {
 			Detail detail = fault.getDetail();
 			return detail.getTextContent();
-		}else{
+		} else {
 			return null;
-			
+
 		}
 	}
-	
+
 	public boolean isBalanced() {
 		return balanced;
 	}
@@ -436,5 +420,5 @@ public class JBFHelper {
 	public void setBalanced(boolean balanced) {
 		this.balanced = balanced;
 	}
-	
+
 }
